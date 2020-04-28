a161bcbe022c3b059bf1baca8375440eb2dc36f4
Salil Gupta <salilgupta@Salils-MacBook-Pro.local>
2020-04-28 13:08:27 -0400

    Updates

---
+++ precache-manifest.60d950ef8dd4900bba780a4a247fd528.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64015e59210cf9e4cb40ffe92437e71f",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "dadb6b9e6db6e091edec",
+    "url": "/Portfolio/static/css/main.5d6aa696.chunk.css"
+  },
+  {
+    "revision": "232399346d42c0baa7cc",
+    "url": "/Portfolio/static/js/2.cbdcefb9.chunk.js"
+  },
+  {
+    "revision": "0778b2003d2fc9a394a3a306079f4922",
+    "url": "/Portfolio/static/js/2.cbdcefb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dadb6b9e6db6e091edec",
+    "url": "/Portfolio/static/js/main.c50393d2.chunk.js"
+  },
+  {
+    "revision": "0b5df8bffa55d17f38f5",
+    "url": "/Portfolio/static/js/runtime-main.da229b30.js"
+  },
+  {
+    "revision": "ad43890c25e4251c47ab5662fab43fa1",
+    "url": "/Portfolio/static/media/Backgroud_image.ad43890c.jpg"
+  },
+  {
+    "revision": "3fb465c9f1a14ee6087922e72f870d47",
+    "url": "/Portfolio/static/media/Salil Gupta_19_03_2020.3fb465c9.pdf"
+  }
+]);
