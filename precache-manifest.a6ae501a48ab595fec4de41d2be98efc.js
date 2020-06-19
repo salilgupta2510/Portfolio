ec6ef7752c5871269c8ebeb7bde3293061a9655c
Salil Gupta <g.salil@live.in>
2020-06-19 13:19:22 +0530

    Updates

--- precache-manifest.a6ae501a48ab595fec4de41d2be98efc.js
+++ precache-manifest.a6ae501a48ab595fec4de41d2be98efc.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "64015e59210cf9e4cb40ffe92437e71f",
+    "revision": "cc30ef8debf06222545d696b7162fcc2",
     "url": "/Portfolio/index.html"
   },
   {
-    "revision": "dadb6b9e6db6e091edec",
+    "revision": "555ed59a12be7a67c310",
     "url": "/Portfolio/static/css/main.5d6aa696.chunk.css"
   },
   {
-    "revision": "232399346d42c0baa7cc",
-    "url": "/Portfolio/static/js/2.cbdcefb9.chunk.js"
+    "revision": "02b17bbe1fc6413942ed",
+    "url": "/Portfolio/static/js/2.0135f8f7.chunk.js"
   },
   {
     "revision": "0778b2003d2fc9a394a3a306079f4922",
-    "url": "/Portfolio/static/js/2.cbdcefb9.chunk.js.LICENSE.txt"
+    "url": "/Portfolio/static/js/2.0135f8f7.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "dadb6b9e6db6e091edec",
-    "url": "/Portfolio/static/js/main.c50393d2.chunk.js"
+    "revision": "555ed59a12be7a67c310",
+    "url": "/Portfolio/static/js/main.b201372a.chunk.js"
   },
   {
     "revision": "0b5df8bffa55d17f38f5",
@@ -28,7 +28,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Portfolio/static/media/Backgroud_image.ad43890c.jpg"
   },
   {
-    "revision": "3fb465c9f1a14ee6087922e72f870d47",
-    "url": "/Portfolio/static/media/Salil Gupta_19_03_2020.3fb465c9.pdf"
+    "revision": "5953af4b93459374b2a94d47144af367",
+    "url": "/Portfolio/static/media/Salil_Gupta_17012020.5953af4b.docx"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.60d950ef8dd4900bba780a4a247fd528.js"
+  "/Portfolio/precache-manifest.a6ae501a48ab595fec4de41d2be98efc.js"
 );
 
 self.addEventListener('message', (event) => {
